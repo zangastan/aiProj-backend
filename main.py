ef87dfc211a28eb454a988ac2d64b4e2c8c8e712
Augustine Kasolota <kasolotaaugustine@gmail.com>
2026-01-07 18:39:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="SmartPurchase System")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
